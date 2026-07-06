cd9c42788a3e0aac631257403ef5bd7e60dc8e78
djmprojectpro-jpg <djmprojectpro@gmail.com>
2026-07-06 14:57:40 -0400

    Fix syntax error in list of options

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,8 +62,7 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
     "📸 Proposals", 
     "📧 Outreach", 
     "📊 Analytics"
-    "🖼️Quote Image Prompt Generator"
-])
+    "🖼️Quote Image Prompt Generator"])
 
 # TAB 1: Leads
 with tab1:
